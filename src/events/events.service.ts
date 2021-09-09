092eca36d433a94597012f4e1f927fff1cdaa736
Razin Idzuddin <razin99@gmail.com>
2021-09-10 06:09:14 +1000

    implemented basic crud services

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { Repository } from 'typeorm';
 import { CreateEventInput } from './dto/create-event.input';
 import { UpdateEventInput } from './dto/update-event.input';
+import { Event } from './entities/event.entity';
 
 @Injectable()
 export class EventsService {
-  create(createEventInput: CreateEventInput) {
-    return 'This action adds a new event';
+  constructor(
+    @InjectRepository(Event) private eventsRepository: Repository<Event>,
+    private usersService: UsersService,
+  ) {}
+
+  create(createEventInput: CreateEventInput): Promise<Event> {
+    const newEvent = this.eventsRepository.create(createEventInput);
+    return this.eventsRepository.save(newEvent);
   }
 
-  findAll() {
-    return `This action returns all events`;
+  findAll(): Promise<Event[]> {
+    return this.eventsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} event`;
+  findOne(id: string): Promise<Event> {
+    return this.eventsRepository.findOne(id);
   }
 
-  update(id: number, updateEventInput: UpdateEventInput) {
-    return `This action updates a #${id} event`;
+  async update(updateEventInput: UpdateEventInput): Promise<Event> {
+    const preUpdateEvent: Event = await this.findOne(updateEventInput.id);
+    return this.eventsRepository.save({
+      ...preUpdateEvent,
+      ...updateEventInput,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} event`;
+  async remove(id: string): Promise<boolean> {
+    return (await this.eventsRepository.delete(id)).affected === 1;
+  }
+
+  async getOrganizer(id: string): Promise<User> {
+    return this.usersService.findOne(id);
   }
 }
