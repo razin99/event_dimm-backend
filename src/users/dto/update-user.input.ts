f7c21786af7ca158c7cd111001019e0fc3f22773
Razin Idzuddin <razin99@gmail.com>
2021-09-10 00:42:48 +1000

    made username and password optional in dto

--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,8 +1,14 @@
 import { CreateUserInput } from './create-user.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, Field, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field(() => Int)
-  id: number;
+  @Field(() => String)
+  id: string;
+
+  @Field(() => String, { nullable: true })
+  username?: string;
+
+  @Field(() => String, { nullable: true })
+  password?: string;
 }
