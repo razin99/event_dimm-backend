60ef7ffa14a599be66e6a2046bb2fc1af372de5b
Razin Idzuddin <razin99@gmail.com>
2021-09-09 23:45:53 +1000

    Added typeorm decorators

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,13 +1,18 @@
 import { ObjectType, Field } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   @Field(() => String)
   id: string;
 
+  @Column()
   @Field(() => String)
   username: string;
 
+  @Column()
   @Field(() => String)
   password: string;
 }
